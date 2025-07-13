b01a7c11fea46127ae11985dd86a5b79533a70e0
zj <zhujun19950922@163.com>
2025-07-14 00:21:42 +0800

    修复 我的训练任务查询

--- sutran-sd-modules/sutran-sd-module-sdapi/src/main/java/com/sutran/sd/sdapi/modules/system/impl/SdTrainPreTaskServiceImpl.java
+++ sutran-sd-modules/sutran-sd-module-sdapi/src/main/java/com/sutran/sd/sdapi/modules/system/impl/SdTrainPreTaskServiceImpl.java
@@ -42,7 +42,7 @@ public class SdTrainPreTaskServiceImpl implements SdTrainPreTaskService {
     @Override
     public Page<SdTrainTask> selectListByUserIdAndNewStatus(Long userId, Integer newStatus, PageQuery pageQuery) {
         LambdaQueryWrapper<SdTrainTask> lqw = new LambdaQueryWrapper<>();
-        lqw.eq(SdTrainTask::getCrtUserId, userId).eq(SdTrainTask::getNewStatus, newStatus).orderByAsc(SdTrainTask::getId);
+        lqw.eq(SdTrainTask::getCrtUserId, userId).eq(newStatus!=null, SdTrainTask::getNewStatus, newStatus).orderByAsc(SdTrainTask::getId);
         return baseMapper.selectPage(pageQuery.build(),lqw);
     }
 

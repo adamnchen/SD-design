14d2f5d0ee3c193ec28672c1b9f92d027bc8c076
zj <zhujun19950922@163.com>
2025-09-17 16:39:47 +0800

    修复 生图次数和训练次数校验

--- sutran-sd-modules/sutran-sd-module-draw/src/main/java/com/sutran/sd/draw/utils/ResultUtil.java
+++ sutran-sd-modules/sutran-sd-module-draw/src/main/java/com/sutran/sd/draw/utils/ResultUtil.java
@@ -76,7 +76,7 @@ public class ResultUtil {
 
                 String viewGridsUrl = viewGridsPath + file.getName();
                 urlList.add(viewGridsUrl);
-                log.warn("[绘图任务]>>>>>>>>xyz plot完成，grid图片原始地址：{},访问地址：{}",file.getPath(),viewGridsUrl);
+                log.warn("[绘图任务]>>>>>>>>xyz plot完成,访问地址：{}",viewGridsUrl);
             } catch (Exception e) {
                 log.error("[绘图任务]>>>>>>>>xyz plot完成，grid图片处理异常：",e);
             }

8c342d7b49113fd187bd443878a8e89b1f6f4c52
阿damn不会写 <2643358194@qq.com>
2025-10-11 17:52:55 +0800

    众筹接口

--- sutran-sd-admin/src/main/java/com/sutran/sd/controller/design/UserModelFileController.java
+++ sutran-sd-admin/src/main/java/com/sutran/sd/controller/design/UserModelFileController.java
@@ -5,6 +5,8 @@ import com.sutran.sd.common.core.domain.R;
 import com.sutran.sd.common.helper.LoginHelper;
 import com.sutran.sd.design.service.ISdUserModelFileService;
 import com.sutran.sd.design.vo.UserModelFileVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.List;
  * @author sutran
  * @date 2025-10-11
  */
+@Tag(name = "我的作品管理", description = "用户生图作品相关接口")
 @Slf4j
 @RestController
 @RequestMapping("/design/user-model-file")
@@ -28,6 +31,7 @@ public class UserModelFileController extends BaseController {
     /**
      * 获取我的作品列表
      */
+    @Operation(summary = "获取我的作品列表", description = "获取当前用户的所有生图作品列表，按创建时间倒序排列")
     @GetMapping("/my-works")
     public R<List<UserModelFileVO>> getMyWorks() {
         try {
@@ -45,6 +49,7 @@ public class UserModelFileController extends BaseController {
     /**
      * 根据分类获取我的作品列表
      */
+    @Operation(summary = "根据分类获取我的作品列表", description = "根据分类获取当前用户的生图作品列表，支持文生图(0)和图生图(1)")
     @GetMapping("/my-works/category/{category}")
     public R<List<UserModelFileVO>> getMyWorksByCategory(@PathVariable Integer category) {
         try {
@@ -67,6 +72,7 @@ public class UserModelFileController extends BaseController {
     /**
      * 获取我的作品详情
      */
+    @Operation(summary = "获取我的作品详情", description = "根据作品ID获取当前用户的生图作品详细信息")
     @GetMapping("/my-works/{id}")
     public R<UserModelFileVO> getMyWorkDetail(@PathVariable Long id) {
         try {
@@ -88,6 +94,7 @@ public class UserModelFileController extends BaseController {
     /**
      * 删除我的作品
      */
+    @Operation(summary = "删除我的作品", description = "根据作品ID删除当前用户的生图作品")
     @DeleteMapping("/my-works/{id}")
     public R<Void> deleteMyWork(@PathVariable Long id) {
         try {
@@ -116,6 +123,7 @@ public class UserModelFileController extends BaseController {
     /**
      * 批量删除我的作品
      */
+    @Operation(summary = "批量删除我的作品", description = "根据作品ID数组批量删除当前用户的生图作品")
     @DeleteMapping("/my-works/batch")
     public R<Void> deleteMyWorks(@RequestBody Long[] ids) {
         try {

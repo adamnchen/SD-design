602d372d66af6ebb062c245ba0be02cc38abe962
zj <zhujun19950922@163.com>
2025-10-23 20:22:53 +0800

    修改编码格式

--- script/sd_proofing_invitations20251022.sql
+++ script/sd_proofing_invitations20251022.sql
@@ -1,4 +1,4 @@
-﻿/*
+/*
  Navicat Premium Dump SQL
 
  Source Server         : mysql
@@ -23,14 +23,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `sd_proofing_invitations`;
 CREATE TABLE `sd_proofing_invitations`  (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '邀请单的唯一ID，主键',
-  `work_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发起邀约的作品ID，关联作品表',
-  `product_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '打样产品的标题',
-  `product_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '邀约作品的详细描述（如材质、尺寸、风格等）',
-  `model_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '模型来源或版本号（例如：v2.3）',
+  `work_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '发起邀约的作品ID，关联作品表',
+  `product_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '打样产品的标题',
+  `product_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '邀约作品的详细描述（如材质、尺寸、风格等）',
+  `model_source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '模型来源或版本号（例如：v2.3）',
   `inviter_user_id` bigint UNSIGNED NOT NULL COMMENT '邀约人用户ID (创建人)',
   `invitee_user_id` bigint UNSIGNED NOT NULL COMMENT '被邀约用户ID',
-  `cooperation_content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '其他合作内容详情',
-  `keywords` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '发起邀约时填写的关键词/标签',
+  `cooperation_content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '其他合作内容详情',
+  `keywords` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '发起邀约时填写的关键词/标签',
   `is_batch_production` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否需要批量生产（0:否, 1:是）',
   `proofing_quantity` int UNSIGNED NOT NULL COMMENT '期望的打样数量 (件)',
   `delivery_limit_hours` int UNSIGNED NOT NULL COMMENT '发起方要求的预计交付时限 (小时)',
@@ -47,14 +47,14 @@ CREATE TABLE `sd_proofing_invitations`  (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间 (邀约时间)',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新时间',
   `draw_number` int NOT NULL COMMENT '打样样品参与抽奖的数量分配给众筹用户,最少一个',
-  `receiver_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '收货人姓名',
-  `receiver_phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '收货人手机号',
-  `receiver_address` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '收货地址',
-  `receiver_area` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '收货地区',
+  `receiver_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货人姓名',
+  `receiver_phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货人手机号',
+  `receiver_address` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货地址',
+  `receiver_area` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '收货地区',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_work_id_status`(`work_id` ASC, `status` ASC) USING BTREE,
   INDEX `idx_inviter_user_id`(`inviter_user_id` ASC) USING BTREE,
   INDEX `idx_invitee_user_id`(`invitee_user_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1978753174349672451 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '作品打样合作邀请记录表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1978753174349672451 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '作品打样合作邀请记录表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;

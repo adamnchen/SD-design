5f47136370be7664a0d84b3fc163f16962182a0b
zj <zhujun19950922@163.com>
2025-11-12 11:38:19 +0800

    修复登录时推送关注微信公众号消息

--- sutran-sd-admin/src/main/java/com/sutran/sd/controller/web/common/MsgController.java
+++ sutran-sd-admin/src/main/java/com/sutran/sd/controller/web/common/MsgController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
